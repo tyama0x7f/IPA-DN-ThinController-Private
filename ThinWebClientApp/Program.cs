22bfb1feedf3812db318a7d6cb7442a4f55ab17d
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2021-03-19 13:34:13 +0900

    ThinWebClientApp 追加

---
+++ ThinWebClientApp/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Buffers;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Serialization;
+using System.Net;
+using System.Net.Sockets;
+
+using System.Security.Cryptography.X509Certificates;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using IPA.Cores.Basic;
+using IPA.Cores.Helper.Basic;
+using static IPA.Cores.Globals.Basic;
+
+using IPA.Cores.Codes;
+using IPA.Cores.Helper.Codes;
+using static IPA.Cores.Globals.Codes;
+
+using IPA.Cores.Web;
+using IPA.Cores.Helper.Web;
+using static IPA.Cores.Globals.Web;
+
+using static IPA.App.ThinWebClientApp.AppGlobal;
+
+// 日本語
+
+namespace IPA.App.ThinWebClientApp
+{
+    // 内部ヘルパー
+    public static class _AppLibHelper
+    {
+        public static readonly string AppThisSourceCodeFileName = Dbg.GetCallerSourceCodeFilePath();
+    }
+
+    // 内部ヘルパー
+    public static partial class AppGlobal
+    {
+        public static ResourceFileSystem AppRes => Res.Codes;
+
+        public static partial class Res
+        {
+            public static readonly ResourceFileSystem Codes = ResourceFileSystem.CreateOrGet(
+                new AssemblyWithSourceInfo(typeof(Res), new SourceCodePathAndMarkerFileName(_AppLibHelper.AppThisSourceCodeFileName, "app_resource_root")));
+        }
+    }
+
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            // ログファイルが何 GB を超えたら自動的に古いものを削除するかの設定
+            CoresConfig.Logger.DefaultAutoDeleteTotalMinSize.Value = 4_000_000_000; // 4GB
+
+            const string appName = "IPA.App.ThinWebClientApp";
+
+            return StandardMainFunctions.DaemonMain.DoMain(
+                new CoresLibOptions(CoresMode.Application,
+                    appName: appName,
+                    defaultDebugMode: DebugMode.Debug,
+                    defaultPrintStatToConsole: false,
+                    defaultRecordLeakFullStack: false),
+                args: args,
+                getDaemonProc: () => new HttpServerDaemon<Startup>(appName, appName, new HttpServerOptions
+                {
+                    HttpPortsList = 80._SingleList(),
+                    HttpsPortsList = 443._SingleList(),
+                    UseKestrelWithIPACoreStack = false,
+                    DebugKestrelToConsole = false,
+                    UseSimpleBasicAuthentication = false,
+                    HoldSimpleBasicAuthenticationDatabase = false,
+                    AutomaticRedirectToHttpsIfPossible = true,
+                    HiveName = "ThinWebClientApiServerForUsers",
+                    DenyRobots = true,
+                    UseGlobalCertVault = false,
+                    ServerCertSelector = (cert, sni) => DevTools.CoresDebugCACert,
+                    MaxRequestBodySize = ThinWebClientConsts.ControllerMaxBodySizeForUsers,
+                    KestrelMaxConcurrentConnections = ThinWebClientConsts.ControllerMaxConcurrentKestrelConnectionsForUsers,
+                    KestrelMaxUpgradedConnections = ThinWebClientConsts.ControllerMaxConcurrentKestrelConnectionsForUsers,
+                    IPv4Only = true,
+                }
+                ));
+        }
+    }
+}
---
+++ ThinWebClientApp/Startup.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using IPA.Cores.Basic;
+using IPA.Cores.Helper.Basic;
+using static IPA.Cores.Globals.Basic;
+
+using IPA.Cores.Web;
+using IPA.Cores.Helper.Web;
+using static IPA.Cores.Globals.Web;
+
+using IPA.Cores.Codes;
+using IPA.Cores.Helper.Codes;
+using static IPA.Cores.Globals.Codes;
+
+using static IPA.App.ThinWebClientApp.AppGlobal;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+#pragma warning disable CS1998 // 非同期メソッドは、'await' 演算子がないため、同期的に実行されます
+
+namespace IPA.App.ThinWebClientApp
+{
+    public class MyThinWebClientHook : ThinWebClientHookBase
+    {
+    }
+
+    public class MyThinWebClientFactory : SharedObjectFactory<ThinWebClient>
+    {
+        public static readonly MyThinWebClientFactory Factory = new MyThinWebClientFactory();
+
+        protected override ThinWebClient CreateNewImpl()
+        {
+            ThinWebClientSettings settings = new ThinWebClientSettings
+            {
+            };
+
+            MyThinWebClientHook hook = new MyThinWebClientHook();
+
+            return new ThinWebClient(settings, hook);
+        }
+    }
+
+    public class Startup
+    {
+        readonly HttpServerStartupHelper StartupHelper;
+        readonly AspNetLib AspNetLib;
+        readonly SharedObjectHolder<ThinWebClient> ThinWebClientHolder;
+
+        public ThinWebClient ThinWebClient => ThinWebClientHolder.Object;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+            // HttpServer ヘルパーの初期化
+            StartupHelper = new HttpServerStartupHelper(configuration);
+
+            // AspNetLib の初期化: 必要な機能のみ ON にすること
+            AspNetLib = new AspNetLib(configuration, AspNetLibFeatures.None);
+
+            // ThinWebClient インスタンスを作成 (Http サーバーのインスタンスが複数存在することを想定しているため、共有させる)
+            this.ThinWebClientHolder = MyThinWebClientFactory.Factory.CreateOrGet();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // AspNetLib による設定を追加
+            AspNetLib.ConfigureServices(StartupHelper, services);
+
+            // 基本的な設定を追加
+            StartupHelper.ConfigureServices(services);
+
+            // Razor ページを追加
+            services.AddRazorPages();
+
+            // MVC 機能を追加
+            services.AddControllersWithViews()
+                .ConfigureMvcWithAspNetLib(AspNetLib);
+
+            // シングルトンサービスの注入
+            services.AddSingleton(ThinWebClient);
+
+            // 全ページ共通コンテキストの注入
+            services.AddScoped<PageContext>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime lifetime)
+        {
+            // wwwroot ディレクトリを static ファイルのルートとして追加
+            StartupHelper.AddStaticFileProvider(Env.AppRootDir._CombinePath("wwwroot"));
+
+            // AspNetLib による設定を追加
+            AspNetLib.Configure(StartupHelper, app, env);
+
+            // 基本的な設定を追加
+            StartupHelper.Configure(app, env);
+
+            // エラーページを追加
+            if (StartupHelper.IsDevelopmentMode)
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            // エラーログを追加
+            app.UseHttpExceptionLogger();
+
+            // Static ファイルを追加
+            app.UseStaticFiles();
+
+            // ルーティングを有効可
+            app.UseRouting();
+
+            // 認証・認可を実施
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            // ルートマップを定義
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            // クリーンアップ動作を定義
+            lifetime.ApplicationStopping.Register(() =>
+            {
+                AspNetLib._DisposeSafe();
+                StartupHelper._DisposeSafe();
+
+                ThinWebClientHolder._DisposeSafe();
+            });
+        }
+    }
+}
