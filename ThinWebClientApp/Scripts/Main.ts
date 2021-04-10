2bde4ff3b05c239d19cde5a5e807a77b2c7c4ff9
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2021-04-10 15:08:45 +0900

    guacamole-common を submodules に移動

--- ThinWebClientApp/Scripts/Main.ts
+++ ThinWebClientApp/Scripts/Main.ts
@@ -16,7 +16,7 @@ import "buefy";
 import { TestClass } from "./submodules/IPA-DN-WebNeko/Scripts/WebNeko";
 
 // Guacamole Library
-import { default as Guacamole } from "./includes/guacamole-common-js/guacamole-common";
+import { default as Guacamole } from "./submodules/IPA-DN-WebNeko/Libraries/guacamole-common-js/guacamole-common";
 
 export function TestFunc(): void
 {
